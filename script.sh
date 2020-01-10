36ac1c0542ca2721434385483bcb5d58a69c2b1a
Clement Magnard <magnar_c@epita.fr>
2020-01-10 12:24:14 +0100

    script: take input file gen google bench
    Signed-off-by: Clement Magnard <magnar_c@epita.fr>

---
+++ script.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/sh
+IFS=
+include=$(cat $1 | grep -Ezo ".*--" | head -n -1)
+code=$(cat $1 | grep -Ezo "\-\-.*" | tail -n +2)
+path="/tmp/bench"
+export INCLUDE=$(echo $include)
+export CODE=$(echo $code)
+cat template.cc | perl -p -e 's/INCLUDE/$ENV{INCLUDE}/g' | perl -p -e 's/CODE/$ENV{CODE}/g'  > $path.cc
+#cat template.cc  | sed "s/CODE/$(echo "'" $code "'")/" > $path.cc
+g++ $path.cc -std=c++11 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o $path
+
+$($path)
