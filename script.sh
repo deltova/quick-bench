ab3ce577838f42cd289d032776a6ae91893d7830
Clement Magnard <magnar_c@epita.fr>
2020-01-13 18:48:27 +0100

    exec: added an option to the script for rep
    Signed-off-by: Clement Magnard <magnar_c@epita.fr>

--- script.sh
+++ script.sh
@@ -1,11 +1,17 @@
 #! /usr/bin/sh
+rep=1
+file=$(echo $1)
+if [[ $1 -eq "-n" ]]; then
+    rep=$(echo $2)
+    file=$3
+fi
 IFS=
-include=$(cat $1 | grep -Ezo ".*--" | head -n -1)
-code=$(cat $1 | grep -Ezo "\-\-.*" | tail -n +2)
+include=$(cat $file | grep -Ezo ".*--" | head -n -1)
+code=$(cat $file | grep -Ezo "\-\-.*" | tail -n +2)
 path="/tmp/bench"
 export INCLUDE=$(echo $include)
 export CODE=$(echo $code)
 cat template.cc | perl -p -e 's/INCLUDE/$ENV{INCLUDE}/g' | perl -p -e 's/CODE/$ENV{CODE}/g'  > $path.cc
 g++ $path.cc -std=c++11 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o $path
 
-$path
+$path --benchmark_repetitions=$rep
